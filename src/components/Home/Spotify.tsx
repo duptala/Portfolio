b3336ce971c63fe59e8ae1a69bf43b2b53bfb632
Devesh Duptala <dduptala@gmail.com>
2024-06-23 05:16:34 +1200

    ✅ Finished Spotify block

--- src/components/Home/Spotify.tsx
+++ src/components/Home/Spotify.tsx
@@ -4,16 +4,22 @@ import { FaSpotify } from "react-icons/fa";
 const Spotify = () => {
   return (
     <div className="w-full h-full bg-spotify rounded-3xl azeret-mono-400 text-xs">
-      <div className="flex flex-col justify-center items-center h-full gap-3">
-        <FaSpotify className="text-7xl text-green-500"></FaSpotify>
-        <p className="dark:text-darkgrey border-2 px-3 py-1 rounded-3xl bg-green-400 border-green-500">
+      <div className="flex flex-col justify-center items-center h-full gap-6 sm:gap-3 md:gap-4 lg:gap-6 xl:gap-3">
+        <FaSpotify className="text-9xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-7xl text-green-500"></FaSpotify>
+        <p className="dark:text-darkgrey border-2 px-3 py-1 rounded-3xl bg-green-400 border-green-500 sm:text-sm text-xl lg:text-xl xl:text-xs">
           Offline
         </p>
 
-        <div className="flex flex-col justify-center text-center">
-          <p className="dark:text-darkgrey text-[0.65rem]">Recently Played:</p>
-          <p className="dark:text-darkgrey font-bold">Kurchi Madathapetti</p>
-          <p className="dark:text-darkgrey">Thaman S.</p>
+        <div className="flex flex-col justify-center text-center gap-2">
+          <p className="dark:text-darkgrey sm:text-xs text-lg xl:text-[0.65rem]">
+            Recently Played:
+          </p>
+          <div>
+            <p className="dark:text-darkgrey font-bold sm:text-sm md:text-lg xl:text-sm text-2xl">
+              Kurchi Madathapetti
+            </p>
+            <p className="dark:text-darkgrey sm:text-sm text-xl">Thaman S.</p>
+          </div>
         </div>
         <div></div>
       </div>
