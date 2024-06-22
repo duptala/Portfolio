6637651962f4d9afff4f8bb372a3ab275b4da238
Devesh Duptala <dduptala@gmail.com>
2024-06-23 05:00:16 +1200

    ✨Added initial Spotify block + minor footer styling adjustments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,47 @@
-import React from 'react'
-import LinkedInLight from '../../public/Linkedin-light.svg'
-import GithubLight from '../../public/Github-light.svg'
-import BehanceLight from '../../public/Behance-light.svg'
-import LinkedInDark from '../../public/Linkedin-dark.svg'
-import GithubDark from '../../public/Github-dark.svg'
-import BehanceDark from '../../public/Behance-dark.svg'
+import React from "react";
+import LinkedInLight from "../../public/Linkedin-light.svg";
+import GithubLight from "../../public/Github-light.svg";
+import BehanceLight from "../../public/Behance-light.svg";
+import LinkedInDark from "../../public/Linkedin-dark.svg";
+import GithubDark from "../../public/Github-dark.svg";
+import BehanceDark from "../../public/Behance-dark.svg";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaBehance } from "react-icons/fa";
-import { Link } from 'react-router-dom'
-
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div className='dark:text-white p-8 flex flex-col justify-center items-center dela-gothic-one-regular text-6xl text-center'>
-            <h1>Devesh Duptala</h1>
-            <p className='azeret-mono-400 text-sm relative md:right-10 mt-5 md:mt-2 text-darkgrey dark:text-midgrey'>All Rights Reserved. 2024</p>
-            <div className='flex flex-row gap-8 mt-5 text-[1.5rem]'>
-                <Link to={"https://www.linkedin.com/in/deveshduptala/"} target="_blank" rel="noopener noreferrer">
-                    <FaLinkedin className=' dark:text-white' />
-                </Link>
-                <Link to={"https://github.com/duptala"} target="_blank" rel="noopener noreferrer">
-                    <FaGithub className=' dark:text-white' />
-                </Link>
-                <Link to={"https://www.behance.net/corzah"} target="_blank" rel="noopener noreferrer">
-                    <FaBehance className=' dark:text-white' />
-                </Link>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="dark:text-white p-8 flex flex-col justify-center items-center dela-gothic-one-regular text-6xl text-center">
+      <h1>Devesh Duptala</h1>
+      <p className="azeret-mono-400 text-sm relative md:right-10 mt-5 md:mt-2 text-darkgrey dark:text-midgrey">
+        All Rights Reserved. 2024
+      </p>
+      <div className="flex flex-row gap-8 mt-5 text-[1.5rem]">
+        <Link
+          to={"https://www.linkedin.com/in/deveshduptala/"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="dark:text-white dark:hover:text-midgrey duration-150 hover:text-midgrey" />
+        </Link>
+        <Link
+          to={"https://github.com/duptala"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub className="dark:text-white dark:hover:text-midgrey duration-150 hover:text-midgrey" />
+        </Link>
+        <Link
+          to={"https://www.behance.net/corzah"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaBehance className="dark:text-white dark:hover:text-midgrey duration-150 hover:text-midgrey" />
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Home/Spotify.tsx
+++ src/components/Home/Spotify.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { FaSpotify } from "react-icons/fa";
 
 const Spotify = () => {
-    return (
-        <div className='w-full h-full bg-spotify rounded-3xl'>
+  return (
+    <div className="w-full h-full bg-spotify rounded-3xl azeret-mono-400 text-xs">
+      <div className="flex flex-col justify-center items-center h-full gap-3">
+        <FaSpotify className="text-7xl text-green-500"></FaSpotify>
+        <p className="dark:text-darkgrey border-2 px-3 py-1 rounded-3xl bg-green-400 border-green-500">
+          Offline
+        </p>
 
+        <div className="flex flex-col justify-center text-center">
+          <p className="dark:text-darkgrey text-[0.65rem]">Recently Played:</p>
+          <p className="dark:text-darkgrey font-bold">Kurchi Madathapetti</p>
+          <p className="dark:text-darkgrey">Thaman S.</p>
         </div>
-    )
-}
+        <div></div>
+      </div>
+    </div>
+  );
+};
 
-export default Spotify
+export default Spotify;
