bd03d56fb28b8064b43670a6e6dd6c162c2eacde
Devesh Duptala <dduptala@gmail.com>
2024-06-23 04:42:36 +1200

    🔧Fixed imports for images from public folder (root)

--- src/components/Home/Intro.tsx
+++ src/components/Home/Intro.tsx
@@ -1,25 +1,33 @@
-import React from 'react'
-import HelloFace from '../../../public/Helloface.png'
+import HelloFace from "/Helloface.png";
 
 const Intro = () => {
-    return (
-        <div className='w-full h-full bg-white dark:bg-darkgrey rounded-3xl grid grid-cols-3'>
-            <div className='hidden md:grid md:col-span-1 justify-center items-center'>
-                <div className='lg:scale-100 xl:scale-75 -rotate-3'>
-                    <img src={HelloFace} alt="HelloFace" />
-                </div>
-            </div>
-            <div className='grid col-span-3 md:col-span-2 azeret-mono-400 p-10'>
-                <h1 className='font-bold text-xl md:text-4xl xl:text-2xl'>Hello &#x1F44B;&#x1F3FE; I'm Devesh! &#129412;</h1>
-                <p className='md:text-md lg:text-xl xl:text-sm text-justify'>
-                    I&apos;m a <span className='text-brightpinktext dark:text-pinktext font-bold'>developer</span> + <span className='text-brightpinktext dark:text-pinktext font-bold'>graphic designer</span>.
-                    Welcome to my space on the internet where you can learn about me :)
-                    I&apos;m passionate about full-stack development, hosting, networking, and design!
-                </p>
-            </div>
+  return (
+    <div className="w-full h-full bg-white dark:bg-darkgrey rounded-3xl grid grid-cols-3">
+      <div className="hidden md:grid md:col-span-1 justify-center items-center">
+        <div className="lg:scale-100 xl:scale-75 -rotate-3">
+          <img src={HelloFace} alt="HelloFace" />
         </div>
-    );
+      </div>
+      <div className="grid col-span-3 md:col-span-2 azeret-mono-400 p-10">
+        <h1 className="font-bold text-xl md:text-4xl xl:text-2xl">
+          Hello &#x1F44B;&#x1F3FE; I'm Devesh! &#129412;
+        </h1>
+        <p className="md:text-md lg:text-xl xl:text-sm text-justify">
+          I&apos;m a{" "}
+          <span className="text-brightpinktext dark:text-pinktext font-bold">
+            developer
+          </span>{" "}
+          +{" "}
+          <span className="text-brightpinktext dark:text-pinktext font-bold">
+            graphic designer
+          </span>
+          . Welcome to my space on the internet where you can learn about me :)
+          I&apos;m passionate about full-stack development, hosting, networking,
+          and design!
+        </p>
+      </div>
+    </div>
+  );
 };
 
-
-export default Intro
+export default Intro;
--- src/components/Home/Location.tsx
+++ src/components/Home/Location.tsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import MapLocation from '../../../public/Location.png'
-import SillyFace from '../../../public/Sillyface.png'
+import MapLocation from "/Location.png";
+import SillyFace from "/Sillyface.png";
 
 const Location = () => {
-    return (
-        <div className='absolute h-full w-full'>
-            <div className='relative w-full h-full bg-gray-600 rounded-3xl'>
-                <img className='absolute inset-0 w-full h-full object-cover rounded-3xl' src={MapLocation} alt="Location of my whereabouts" />
-                <div className='absolute inset-0 flex items-center justify-center h-fit'>
-                    <div className='absolute top-10 right-4 transform scale-[0.4] rounded-full bg-white p-1 shadow-xl border-black border-4'>
-                        <img className='rounded-full' src={SillyFace} alt="A picture of me on the map!" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="absolute h-full w-full">
+      <div className="relative w-full h-full bg-gray-600 rounded-3xl">
+        <img
+          className="absolute inset-0 w-full h-full object-cover rounded-3xl"
+          src={MapLocation}
+          alt="Location of my whereabouts"
+        />
+        <div className="absolute inset-0 flex items-center justify-center h-fit">
+          <div className="absolute top-10 right-4 transform scale-[0.4] rounded-full bg-white p-1 shadow-xl border-black border-4">
+            <img
+              className="rounded-full"
+              src={SillyFace}
+              alt="A picture of me on the map!"
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-export default Location
+export default Location;
