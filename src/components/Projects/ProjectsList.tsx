596674f6137e793cee17fddd4e48af0af9bcd6e2
Devesh Duptala <96435676+duptala@users.noreply.github.com>
2024-06-24 19:46:16 +1200

    � Refined projects description

--- src/components/Projects/ProjectsList.tsx
+++ src/components/Projects/ProjectsList.tsx
@@ -16,15 +16,15 @@ const projects: ProjectData[] = [
   {
     title: "AUIS Website",
     description:
-      "Auckland University Indian Society is a univeristy group related project to create a website for the AUIS club which handles events, payments etc. Over 500+ users to be used.",
+      `A website developed in a team of 9 for the Auckland University Indian Society (AUIS) that manages social events, processes event payments, and supports over 500 members.`,
     date: "2024",
     image: "../../../projects/auis.png",
     in_progress: true,
-    tech_stack: ["React", "Tailwind", "Strapi", "Stripe", "PostgreSQL"],
+    tech_stack: ["React", "Tailwind", "Strapi", "Stripe", "PostgreSQL", "Docker"],
   },
   {
     title: "Portfolio",
-    description: `This very site you're seeing was done to showcase all my work in one tidy space on the web :)`,
+    description: `My personal website that organizes and showcases all my projects and work in one tidy place on the web :)`,
     date: "2024",
     image: "../../../projects/portfolio.png",
     link: "https://deveshduptala.com",
@@ -34,10 +34,7 @@ const projects: ProjectData[] = [
   },
   {
     title: "Course Unbox",
-    description: `CourseUnbox is a personal project that I made 
-        where students can online review platform
-        I made for students to be able to leave reviews
-        for university courses. Over 30+ users.`,
+    description: `An online platform where university students can review and share their course experiences, with over 10 active users.`,
     date: "2024",
     image: "../../../projects/courseunbox.png",
     link: "https://courseunbox.co.nz",
@@ -46,10 +43,7 @@ const projects: ProjectData[] = [
   },
   {
     title: "StockQuest",
-    description: `A university group project which is aimed to 
-make a gamified version of the stock market 
-to allow users to join, create games and battle 
-it out real time.`,
+    description: `A university project to create a gamified stock market simulation. Users can join, create games, and compete in real-time to learn about stock trading.`,
     date: "2024",
     image: "../../../projects/stockquest.png",
     link: "https://stockquest.app",
@@ -58,10 +52,7 @@ it out real time.`,
   },
   {
     title: "Skechers Shoe Tracker",
-    description: `An online management tool for store to manage
-inventory of items for quicker retrieval on 
-shop floor. It was used internally by staff 
-members.`,
+    description: `An inventory management tool developed for the Skechers store where I worked. It helps staff quickly find and manage shoe locations, making it easier to assist customers and manage the store.`,
     date: "2023",
     image: "../../../projects/skechersshoetracker.png",
     link: "http://skechersshoetracker.onrender.com/",
@@ -71,10 +62,7 @@ members.`,
   },
   {
     title: "PC Remote Controller",
-    description: `A tool which you can connect to pc running the
-script through local area network (LAN) to 
-control playback (play, pause, forward etc.). 
-Used by friends and co.`,
+    description: `A tool to control media playback on a PC via a local network (LAN). It allows users to play, pause, skip media, and adjust volume. Currently used by friends.`,
     date: "2023",
     image: "../../../projects/pcremotecontrol.png",
     github_link: "https://github.com/duptala/PC-remote-control",
@@ -83,10 +71,7 @@ Used by friends and co.`,
   },
   {
     title: "Keyboard Marketplace",
-    description: `An android application that focuses on a 
-simplified e-commerce platform but for android. 
-Our theme was keyboards so we made a demo 
-application for it.`,
+    description: `A university project where my group of three developed an Android app for a simplified e-commerce platform focused on keyboards.`,
     date: "2023",
     image: "../../../projects/keyboardmarketplace.png",
     in_progress: false,
