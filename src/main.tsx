eef03507f6dab02ea630e2abbefd193bc80e213f
Devesh Duptala <dduptala@gmail.com>
2024-06-25 04:42:43 +1200

    ✨Added react-helmet for SEO optization

--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,53 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
-import Home from './pages/Home.tsx';
-import Projects from './pages/Projects.tsx';
-import Designs from './pages/Designs.tsx';
-import Blog from './pages/Blog.tsx';
-import AboutMe from './pages/AboutMe.tsx';
-import ErrorPage from './pages/ErrorPage.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home.tsx";
+import Projects from "./pages/Projects.tsx";
+import Designs from "./pages/Designs.tsx";
+import Blog from "./pages/Blog.tsx";
+import AboutMe from "./pages/AboutMe.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
+import { HelmetProvider } from "react-helmet-async";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       {
-        path: '',
+        path: "",
         element: <Home />,
       },
       {
-        path: 'projects',
+        path: "projects",
         element: <Projects />,
       },
       {
-        path: 'designs',
+        path: "designs",
         element: <Designs />,
       },
       {
-        path: 'blog',
+        path: "blog",
         element: <Blog />,
       },
       {
-        path: 'aboutme',
+        path: "aboutme",
         element: <AboutMe />,
       },
       {
-        path: '*',
+        path: "*",
         element: <ErrorPage />, // this redirects all unknown routes to the error page
       },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  </React.StrictMode>
+);
