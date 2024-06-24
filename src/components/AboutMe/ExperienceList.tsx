77c89636e1619709e288c2e50f54704920c87e81
Devesh Duptala <96435676+duptala@users.noreply.github.com>
2024-06-24 23:39:15 +1200

    ✨ Added Experience component and loaded with experiences

--- src/components/AboutMe/ExperienceList.tsx
+++ src/components/AboutMe/ExperienceList.tsx
@@ -13,11 +13,11 @@ interface IExperience {
 const experiences: IExperience[] = [
     {
         title: "Web Development Consulting Club (WDCC)",
-        role: "Full Stack Developer",
+        role: "Full Stack Developer •",
         start_date: "MAR 2024",
         present: true,
-        responsibilities: `
-        • Currently collaborating in a team of 9 to develop a website for AUIS, designed to serve over 500 university students.
+        responsibilities: `• Currently collaborating in a team of 9 to develop a website for AUIS, designed to serve over 500 university students.
+        
         • Leading the frontend design and contributing to the implementation of various features, with project completion expected by the end of the year.`
     },
     {
@@ -25,8 +25,8 @@ const experiences: IExperience[] = [
         role: "Graphic Designer •",
         start_date: "JAN 2024",
         present: true,
-        responsibilities: `
-        • Designed visual content to promote and encourage participation in club events, including social media posts and event posters.
+        responsibilities: `• Designed visual content to promote and encourage participation in club events, including social media posts and event posters.
+        
         • Assisted in the organization and execution of various club events.`
     },
     {
@@ -35,9 +35,10 @@ const experiences: IExperience[] = [
         start_date: "NOV 2023",
         present: false,
         end_date: "FEB 2024",
-        responsibilities: `
-        •Led projects focusing on web scraping, computer vision, and Azure Cloud Services to improve data extraction, infrastructure analysis, and operational efficiency
+        responsibilities: `•Led projects focusing on web scraping, computer vision, and Azure Cloud Services to improve data extraction, infrastructure analysis, and operational efficiency
+        
         •Utilised data scraping for traffic analysis and automation to enhance bridge standards, contributing to more effective management strategies
+        
         •Conducted advanced data analysis and visualisation with Python, supporting business decision-making
         by analysing public contracts`
     },
@@ -47,21 +48,22 @@ const experiences: IExperience[] = [
         start_date: "NOV 2022",
         present: false,
         end_date: "NOV 2023",
-        responsibilities: `
-        • Provided prompt and friendly customer service.
+        responsibilities: `• Provided prompt and friendly customer service.
+        
         • Enhanced communication and interpersonal skills.
+        
         • Effectively addressed and resolved customer concerns.`
     }
 ]
 
 const ExperienceList = () => {
     return (
-        {
-            experiences.map((experience) => {
-                <Experience></Experience>
-            })
-        }
-    )
-}
+        <>
+            {experiences.map((experience, index) => (
+                <Experience key={index} experience={experience} />
+            ))}
+        </>
+    );
+};
 
 export default ExperienceList
