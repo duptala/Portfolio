23a4f483429491e4c54227ca461d4d3aba8267e2
Devesh Duptala <dduptala@gmail.com>
2024-06-26 01:10:07 +1200

    🔧Attempted to optimize the map image which is throttling mobile devices

--- src/components/Home/Location.tsx
+++ src/components/Home/Location.tsx
@@ -1,15 +1,28 @@
-import MapLocation from "/Location.webp";
 import SillyFace from "/Sillyface.webp";
+import AucklandMap from "/location/Location_lwa4hg_c_scale,w_200.webp";
 
 const Location = () => {
   return (
     <div className="absolute h-full w-full">
       <div className="relative w-full h-full bg-gray-600 rounded-3xl">
-        <img
-          className="absolute inset-0 w-full h-full object-cover rounded-3xl"
-          src={MapLocation}
-          alt="Location of my whereabouts"
-        />
+        <picture>
+          <source
+            srcSet="
+              location/Location_lwa4hg_c_scale,w_200.webp 200w,
+              location/Location_lwa4hg_c_scale,w_416.webp 416w,
+              location/Location_lwa4hg_c_scale,w_575.webp 575w,
+              location/Location_lwa4hg_c_scale,w_718.webp 718w,
+              location/Location_lwa4hg_c_scale,w_917.webp 917w,
+              location/Location_lwa4hg_c_scale,w_1021.webp 1021w"
+            sizes="(max-width: 1021px) 100vw, 1021px"
+            type="image/webp"
+          />
+          <img
+            className="absolute inset-0 w-full h-full object-cover rounded-3xl"
+            src={AucklandMap}
+            alt="Location of Auckland"
+          />
+        </picture>
         <div className="absolute inset-0 flex items-center justify-center h-fit">
           <div className="absolute top-10 right-4 transform scale-[0.4] rounded-full bg-white p-1 drop-shadow-xl shadow-xl border-black border-4">
             <img
